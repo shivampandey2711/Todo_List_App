047d0086f618b50e8e419030c0b20bc6665896dd
shivam pandey <105489916+shivampandey2711@users.noreply.github.com>
2023-09-27 09:24:16 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,20 @@
+// Header.js
+import React from 'react';
+
+function Header({ loggedIn, currentUser, onLogout }) {
+  return (
+    <header>
+      <h1>Todo App</h1>
+      {loggedIn ? (
+        <div>
+          Welcome, {currentUser}! <button onClick={onLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </header>
+  );
+}
+
+export default Header;
 
