4bd9b9bea35a281f6a5cd83e34e07d64904bc4f5
shivam pandey <105489916+shivampandey2711@users.noreply.github.com>
2023-09-27 09:25:17 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,48 @@
+// TodoList.js
+import React, { useState } from 'react';
+import Task from './Task';
+
+function TodoList({ currentUser }) {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    // Implement task creation and storage logic here
+  };
+
+  const deleteTask = (taskId) => {
+    // Implement task deletion logic here
+  };
+
+  const completeTask = (taskId) => {
+    // Implement task completion logic here
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="New Task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={deleteTask}
+            onComplete={completeTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
 
