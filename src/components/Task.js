ba954eaf7fc9fda728404928fac4ce87f77c3faa
shivam pandey <105489916+shivampandey2711@users.noreply.github.com>
2023-09-27 09:26:01 +0530

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,1 +1,21 @@
+// Task.js
+import React from 'react';
+
+function Task({ task, onDelete, onComplete }) {
+  return (
+    <li>
+      <span
+        style={{ textDecoration: task.completed ? 'line-through' : 'none' }}
+      >
+        {task.title}
+      </span>
+      <button onClick={() => onComplete(task.id)}>
+        {task.completed ? 'Undo' : 'Complete'}
+      </button>
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </li>
+  );
+}
+
+export default Task;
 
