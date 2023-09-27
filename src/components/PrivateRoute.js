452258e9df04d2874d196fc7143f116b284da7eb
shivam pandey <105489916+shivampandey2711@users.noreply.github.com>
2023-09-27 09:25:40 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,26 @@
+// PrivateRoute.js
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function PrivateRoute({ children, loggedIn, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        loggedIn ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
 
